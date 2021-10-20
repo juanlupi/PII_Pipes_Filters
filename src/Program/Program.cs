c62113f747768a5829e1d249ba59c5a87ec29103
Juan <juan.lupi@correo.ucu.edu.uy>
2021-10-20 15:34:49 -0300

    Twitter publications

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using CompAndDel.Pipes;
 using CompAndDel.Filters;
+using TwitterUCU;
 
 namespace CompAndDel
 {
@@ -8,6 +9,28 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
+            var twitter = new TwitterImage();
+
+            PictureProvider provider = new PictureProvider();
+            IPicture picture = provider.GetPicture(@"luke.jpg");
+
+            FilterNegative filternegative = new FilterNegative();
+            FilterGreyscale filtergreyscale = new FilterGreyscale();
+
+            PipeNull pipenull = new PipeNull();
+            PipeSerial pipeserial2 = new PipeSerial(filternegative, pipenull);
+            PipeSerial pipeserial1 = new PipeSerial(filtergreyscale, pipeserial2);
+
+            IPicture image1 = pipeserial1.Send(picture);
+            IPicture image2 = pipeserial2.Send(image1);
+            IPicture image3 = pipenull.Send(image2);
+
+            provider.SavePicture(image1, @"greyandnegativefilters.jpg");
+            provider.SavePicture(image3, @"greyfilter.jpg");
+
+            Console.WriteLine(twitter.PublishToTwitter("Grey Luke", @"greyfilter.jpg"));
+            Console.WriteLine(twitter.PublishToTwitter("Grey and negative Luke", @"greyandnegativefilters.jpg"));
+
         }
     }
 }
